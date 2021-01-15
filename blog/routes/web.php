77220b028bebacb5706963ab91b9caa3008a7fad
yuukioda999 <oda.yuuki@acrovision.jp>
2021-01-15 09:48:37 +0900

    ブログ一覧、登録、更新追加

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -11,6 +11,22 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//ブログ一覧表示
+Route::get('/','BlogController@showList')->name('blogs');
+
+//ブログ登録画面表示
+Route::get('/blog/create','BlogController@showCreate')->name('create');
+
+//ブログ登録
+Route::post('/blog/store','BlogController@exeStore')->name('store');
+
+//ブログ詳細表示
+Route::get('/blog/{id}','BlogController@showDetail')->name('show');
+
+//ブログ編集画面を表示
+Route::get('/blog/edit/{id}','BlogController@showEdit')->name('edit');
+//ブログ編集
+Route::post('/blog/update','BlogController@exeUpdate')->name('update');
+
+
+
